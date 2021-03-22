231856044796b7957831972fe7a77ea2ed259b06
jinxyang <jinx@jinxyang.com>
2021-03-22 13:56:19 +0800

    feat: add babel-plugin-styled-components

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,18 @@ module.exports = (api) => {
   api.cache(true)
 
   const presets = ['@babel/env', '@babel/react']
+  const plugins = [
+    [
+      'babel-plugin-styled-components',
+      {
+        displayName: true,
+        ssr: false,
+      },
+    ],
+  ]
 
   return {
-    presets
+    presets,
+    plugins,
   }
 }
